bfe4ad3c966312ade75ab2e80560d18692f647d2
HarDevSan <hardevsandhu@hotmail.de>
2020-06-19 08:52:35 +0200

    add StartMenu Manager script

---
+++ Assets/_Scripts/StartMenuManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartMenuManager : MonoBehaviour
+{
+
+    [Header("MainCanvas")]
+    public CanvasGroup canvasMain;
+    [Header("MainMenuCanvasGroup")]
+    public CanvasGroup mainMenuGroup;
+
+
+    public delegate void PlayButtonClicked();
+    public static event PlayButtonClicked OnPlayButtonClicked;
+
+    public void OnPlayClicked()
+    {
+        Debug.Log("Play button was clicked");
+        canvasMain.alpha = 0;
+        OnPlayButtonClicked.Invoke();
+        InputReceiver.UnBlockMovementInputs();
+        PlayerController.isApplyGravity = true;
+    }
+
+}
