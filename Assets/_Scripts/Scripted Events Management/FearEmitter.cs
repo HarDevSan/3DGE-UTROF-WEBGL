6dca2b431e5965ddcb6cd46615d2dfbf4c47fb12
HarDevSan <hardevsandhu@hotmail.de>
2020-06-27 15:45:46 +0200

    remove unneeded lists from fear emiter scrpt

--- Assets/_Scripts/Scripted Events Management/FearEmitter.cs
+++ Assets/_Scripts/Scripted Events Management/FearEmitter.cs
@@ -7,15 +7,9 @@ using UnityEngine.Events;
 public class FearEmitter : MonoBehaviour
 {
 
-    [Header("LightList")]
-    public List<Light> lightList = new List<Light>();
-
-    [Header("AudioSourcetList")]
-    public List<AudioSource> audioSourceList = new List<AudioSource>();
-
     public UnityEvent playerEntersEmitter;
 
-    private void OnTriggerStay(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
         Debug.Log("Player entered Emitter");
         playerEntersEmitter.Invoke();
