241a610da64eaffee808af336b20197d4c3283c6
HarDevSan <hardevsandhu@hotmail.de>
2020-06-27 23:29:45 +0200

    implement audio manager instance

--- Assets/_Scripts/Audio Management/AudioManager_Instance.cs
+++ Assets/_Scripts/Audio Management/AudioManager_Instance.cs
@@ -2,17 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class AudioManager_Instance : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    AudioSource source;
+
+    private void Start()
     {
-        
+        source = GetComponent<AudioSource>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PlayAudioSource()
     {
-        
+        source.PlayOneShot(source.clip);
     }
 }
