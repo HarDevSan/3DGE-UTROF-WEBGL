e872e5e5ff4a43f9848cc6dd4d4c9db00d5e8516
HarDevSan <hardevsandhu@hotmail.de>
2020-06-28 23:46:35 +0200

    add textevent script

---
+++ Assets/_Scripts/Interactivity/TextEvent.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextEvent : MonoBehaviour
+{
+    public TMPro.TextMeshProUGUI textToDisplay;
+
+    private void Awake()
+    {
+        PlayerController.OnPlayerSeesSomethingInteractable_Item += ShowText;
+        PlayerController.OnPlayerDoesNotSeeSomehtingInteractable += HideText;
+    }
+
+    void ShowText()
+    {
+        textToDisplay.alpha = 1;
+    }
+
+    void HideText()
+    {
+        textToDisplay.alpha = 0;
+
+    }
+}
