a5ad69d439f96c767baa5e2f07e6b619acdcfbb9
HarDevSan <hardevsandhu@hotmail.de>
2020-07-19 23:33:27 +0200

    add AssetUnloader

---
+++ Assets/_Scripts/Scene and Persistence Handling/AssetUnloader.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.ResourceManagement;
+
+public class AssetUnloader : MonoBehaviour
+{
+    private void Awake()
+    {
+        SceneLoader.OnScene_Has_UnLoaded += UnloadAllUnusedAssets;
+    }
+
+    void UnloadAllUnusedAssets()
+    {
+        StartCoroutine(UnloadAllAssetsRoutine());
+    }
+
+    IEnumerator UnloadAllAssetsRoutine()
+    {
+        AsyncOperation unloadOperation = Resources.UnloadUnusedAssets();
+
+        while (unloadOperation.isDone == false)
+        {
+            Debug.Log("UnLOading all Assets");
+            yield return null;
+        }
+        Debug.Log("UnLOading all Assets is Done");
+    }
+}
