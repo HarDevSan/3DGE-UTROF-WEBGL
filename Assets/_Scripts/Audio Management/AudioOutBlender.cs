238de31523158e89f6210159b7f0a6cffcd0ef67
HarDevSan <hardevsandhu@hotmail.de>
2020-08-04 00:22:26 +0200

    add AudioOutBlender class

---
+++ Assets/_Scripts/Audio Management/AudioOutBlender.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioOutBlender : MonoBehaviour
+{
+    public float blendOutSpeed;
+
+    public AudioSource introAudioSource;
+
+
+    private void Awake()
+    {
+        TextEvent_Sequential.OnAllTextHasBeenPrinted += BlendOutGroup;
+    }
+
+    void BlendOutGroup()
+    {
+        StartCoroutine(BlendOutGroupRoutine());
+    }
+
+    IEnumerator BlendOutGroupRoutine()
+    {
+
+        while (introAudioSource.volume > 0.005)
+        {
+            introAudioSource.volume = Mathf.Lerp(introAudioSource.volume, 0, blendOutSpeed * Time.deltaTime );
+         
+            yield return null;
+        }
+
+        Debug.Log("Reached BlendOut");
+
+    }
+
+}
