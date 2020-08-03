e63a2d3ec89ad57901399118d029d28ede2383c3
HarDevSan <hardevsandhu@hotmail.de>
2020-08-03 08:15:33 +0200

    add textinterpolate

---
+++ Assets/_Scripts/Interactivity/TextInterpolate.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TextInterpolate : MonoBehaviour
+{
+    public TextMeshProUGUI textToBlendIn;
+    public float blendInAmount;
+
+    public bool shouldBlendIn;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        TextEvent_Sequential.OnFirstTextHasBeenPrinted += BLendInText;
+    }
+
+
+
+    public void BLendInText()
+    {
+        if (shouldBlendIn)
+        {
+            StartCoroutine(BlendInTextRoutine());
+
+        }
+    }
+    
+    IEnumerator BlendInTextRoutine()
+    {
+        while(textToBlendIn.alpha < 1)
+        {
+            textToBlendIn.alpha = Mathf.Lerp(textToBlendIn.alpha , 1,  blendInAmount * Time.time);
+
+            yield return null;
+        }
+
+    }
+}
