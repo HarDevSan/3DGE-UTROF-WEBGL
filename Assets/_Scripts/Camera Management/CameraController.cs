ccdba381f7420f591a39eabfb6815f05c791ffd7
HarDevSan <hardevsandhu@hotmail.de>
2020-08-03 08:09:24 +0200

    update camcontroller

--- Assets/_Scripts/Camera Management/CameraController.cs
+++ Assets/_Scripts/Camera Management/CameraController.cs
@@ -6,27 +6,62 @@ using Cinemachine;
 public class CameraController : MonoBehaviour
 {
 
-     CinemachineFreeLook vFreeLookCam;
+     public CinemachineFreeLook vFreeLookCam;
+    public CinemachineCollider vFreeLookCamCollider;
 
     [Range(.5f, 1)]
     public float clampYFactorPositve;
     [Range(0f,.5f)]
     public float clampYFactorNegative;
 
-
-
+    /*Fields for camra behaviour, as the cemarea target lookAt transform is slightly offset on negtive Z-axis,
+     * this causes problems with the Cinemachine Collider Strategy. Therefore, the first frame the vCam gets occluded,
+     * I switch the target to one that is centered inside the players body andhas no offset. This will cause Cinemachines
+     * collision strategy to work as intended. -- Does not work, no acces to ask if camera is occluded*/
+    [Header("Transfom when cam is not-occluded")]
+    public Transform transformWheNotOccluded;
+    [Header("Transfom when cam is occluded")]
+    public Transform transformWhenIsOccluded;
 
     // Start is called before the first frame update
     void Start()
     {
-        vFreeLookCam = GetComponent<CinemachineFreeLook>();
+        //vFreeLookCam = GetComponent<CinemachineFreeLook>();
+        //vFreeLookCamCollider = vFreeLookCam.GetComponent<CinemachineCollider>();
+
+    }
+
+    void SwapTargets(Transform target, Vector3 PostionDelta)
+    {
+
     }
 
     // Update is called once per frame
     void Update()
     {
+       
+        if(vFreeLookCam != null)
+        {
+            Debug.Log("Cam found!");
+        }
+
+        /*Check each frame if camera is occluded, if it is, swp target to non offset target. 
+        Otherwise, kep the centered one for the time of the occlusion.*/
+        if (vFreeLookCamCollider.IsTargetObscured(vFreeLookCam))
+        {
+            Debug.Log("Cam is Occluded");
+            vFreeLookCam.m_LookAt = transformWhenIsOccluded;
+        }
+        else
+        {
+            vFreeLookCam.m_LookAt = transformWheNotOccluded;
+            Debug.Log("Cam is NOT Occluded");
+
+        }
+
 
         float axis = vFreeLookCam.m_YAxis.Value;
+
         if(axis > clampYFactorPositve)
         {
             float lastpos = vFreeLookCam.m_YAxis.Value;
@@ -42,6 +77,11 @@ public class CameraController : MonoBehaviour
 
         }
         vFreeLookCam.m_YAxis.Value = axis;
+
+    }
+    void SwitchLookAtTargetsOnOcclusion() {
+
+                
 
     }
 }
