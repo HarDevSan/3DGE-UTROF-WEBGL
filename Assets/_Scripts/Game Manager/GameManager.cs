5bb74b0eaad0f0486230a32cc28a1045c63e3003
HarDevSan <hardevsandhu@hotmail.de>
2020-08-05 20:38:03 +0200

    update GameManager class

--- Assets/_Scripts/Game Manager/GameManager.cs
+++ Assets/_Scripts/Game Manager/GameManager.cs
@@ -26,6 +26,7 @@ public class GameManager : MonoBehaviour
         //Cursor.lockState = CursorLockMode.Locked;
         InputReceiver.On_P_Input += StopGame;
         isCharControllerEnabled = true;
+        
     }
 
     private void Update()
