5a3bc1adfd4b6b6e047211400514c8edbad21da5
HarDevSan <hardevsandhu@hotmail.de>
2020-08-06 19:03:16 +0200

    add inventoryItem script

---
+++ Assets/_Scripts/Inventory Management/InventoryItem.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class InventoryItem : MonoBehaviour
+{
+    [SerializeField]
+    bool isNearItem;
+
+    public Inventory inventorSO;
+
+    public string itemName;
+
+    public GameObject inventoryObject;
+
+    public AudioSource inventoryItemSound;
+
+    private void Update()
+    {
+        if (isNearItem)
+        {
+            if (InputReceiver.CheckIf_Use_Pressed())
+            {
+                AddItemToInventory(itemName);
+                inventoryObject.SetActive(false);
+                //inventoryItemSound.PlayOneShot(inventoryItemSound.clip);
+            }
+        }
+    }
+
+
+    public void AddItemToInventory(string name)
+    {
+        inventorSO.AddItemToList(name);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        isNearItem = true;
+    }
+}
