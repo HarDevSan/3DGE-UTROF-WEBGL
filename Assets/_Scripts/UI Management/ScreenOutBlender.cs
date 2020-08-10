5a4c141a7a7f311afa726f93ccec62ba16369401
HarDevSan <hardevsandhu@hotmail.de>
2020-08-10 22:45:04 +0200

    add class to blend out screen

--- Assets/_Scripts/UI Management/ScreenOutBlender.cs
+++ Assets/_Scripts/UI Management/ScreenOutBlender.cs
@@ -1,14 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ScreenOutBlender : MonoBehaviour
 {
-    public float blendOutSpeed;
+    public float blendOutGroupSpeed;
+    public float blendOutPromptSayingPressESpeed;
 
     public CanvasGroup groupToBlendOut;
-
-    float perc;
+    public TextMeshProUGUI pressEpromptToBlendOut;
 
     public delegate void SreenFinsihedBeingBlendedOut();
     public static event SreenFinsihedBeingBlendedOut OnScreenFinsihedBeingBlendedOut;
@@ -16,21 +17,44 @@ public class ScreenOutBlender : MonoBehaviour
     private void Awake()
     {
         TextEvent_Sequential.OnAllTextHasBeenPrinted += BlendOutGroup;
+        TextEvent_Sequential.OnAllTextHasBeenPrinted += BlendOutPromptSayingPressE;
+
     }
 
     void BlendOutGroup()
     {
         StartCoroutine(BlendOutGroupRoutine());
+
+    }
+
+    void BlendOutPromptSayingPressE()
+    {
+        StartCoroutine(BlendOutpromptSayingPressERoutine());
     }
 
     IEnumerator BlendOutGroupRoutine()
     {
 
 
-        while (groupToBlendOut.alpha > 0.005)
+        while (groupToBlendOut.alpha > 0.01)
         {
-            groupToBlendOut.alpha = Mathf.Lerp(groupToBlendOut.alpha, 0, blendOutSpeed * Time.deltaTime);
+            groupToBlendOut.alpha = Mathf.Lerp(groupToBlendOut.alpha, 0, blendOutGroupSpeed * Time.deltaTime);
          
+            yield return null;
+        }
+
+        Debug.Log("Reached BlendOut");
+        OnScreenFinsihedBeingBlendedOut.Invoke();
+
+    }
+    IEnumerator BlendOutpromptSayingPressERoutine()
+    {
+
+
+        while (pressEpromptToBlendOut.alpha > 0.01)
+        {
+            pressEpromptToBlendOut.alpha = Mathf.Lerp(pressEpromptToBlendOut.alpha, 0, blendOutPromptSayingPressESpeed * Time.deltaTime);
+
             yield return null;
         }
 
