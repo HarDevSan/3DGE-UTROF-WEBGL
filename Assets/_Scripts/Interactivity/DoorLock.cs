42c1e4dbe23dd9b626b4723155216c3eda5f31be
HarDevSan <hardevsandhu@hotmail.de>
2020-08-10 22:40:33 +0200

    add doorlock script

---
+++ Assets/_Scripts/Interactivity/DoorLock.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorLock : MonoBehaviour
+{
+    public Inventory inventory;
+
+    private void OnTriggerStay(Collider other)
+    {
+        
+    }
+
+}
