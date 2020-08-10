9875f1c643877515433a8f573cc6295e66bde239
HarDevSan <hardevsandhu@hotmail.de>
2020-08-10 22:41:09 +0200

    add inventory resetter script

---
+++ Assets/_Scripts/Inventory Management/InventoryResetter.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryResetter : MonoBehaviour
+{
+    public Inventory inventoryToResetSO;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        inventoryToResetSO.itemList = new List<string>();
+    }
+
+   
+}
