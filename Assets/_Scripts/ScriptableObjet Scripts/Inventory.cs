d9316bdae7e7e73a082a9aa5d900ec2dacad755f
HarDevSan <hardevsandhu@hotmail.de>
2020-08-10 22:51:36 +0200

    add scriptable object script for inventory

---
+++ Assets/_Scripts/ScriptableObjet Scripts/Inventory.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu(fileName = "Inventory", menuName = "ScriptableObjects/Inventory", order = 1)]
+public class Inventory : ScriptableObject
+{
+    public List<string> itemList;
+
+    private void Awake()
+    {
+        //ClearInvenotryWhenGameStarts
+        itemList = new List<string>();
+     
+    }
+
+    public bool SearchListFor(string name)
+    {
+        for (int i = 0; i < itemList.Count; i++)
+        {
+            if (itemList[i].Equals(name))
+            {
+                return true;
+
+            }
+            else
+            {
+                return false;
+            }
+            
+
+        }
+
+        return false;
+    }
+
+    public void AddItemToList(string name)
+    {
+        Debug.Log("Added item to inventory called : " + name);
+        itemList.Add(name);
+    }
+
+}
+
