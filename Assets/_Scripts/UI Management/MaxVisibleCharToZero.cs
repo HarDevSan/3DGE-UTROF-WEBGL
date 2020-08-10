81c30769a8af62db7bef10366a788d7193236039
HarDevSan <hardevsandhu@hotmail.de>
2020-08-10 22:37:45 +0200

    add maxVisCharToZero script

---
+++ Assets/_Scripts/UI Management/MaxVisibleCharToZero.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MaxVisibleCharToZero : MonoBehaviour
+{
+    public TextMeshProUGUI[] textsToResetMaxVisCharToZeroOnStart;
+    //public CanvasGroup[] textsToZeroOutAlpha;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        //textsToZeroOutAlpha = FindObjectsOfType<TextMeshProUGUI>();
+        foreach (TextMeshProUGUI txt in textsToResetMaxVisCharToZeroOnStart)
+        {
+            txt.maxVisibleCharacters = 0;
+        }
+        //foreach (CanvasGroup txt in textsToZeroOutAlpha)
+        //{
+        //    txt.alpha = 0;
+        //}
+    }
+}
