c2333b4e8c18bd0e3a583d346d20a6954abe344d
HarDevSan <hardevsandhu@hotmail.de>
2020-09-04 20:25:52 +0200

    add Scriptable Object UI Blending script

---
+++ Assets/_Scripts/ScriptableObjet Scripts/UIBlending.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu(fileName = "UIBLending", menuName = "ScriptableObjects/UI", order = 1)]
+public class UIBlending : ScriptableObject
+{
+    public float toSettingsBlendTime;
+    public float blendOutSettingstime;
+
+
+}
+
