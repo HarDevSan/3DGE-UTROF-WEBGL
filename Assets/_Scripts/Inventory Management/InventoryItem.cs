12ae5ac608d68dbb1cce7ac391dfac1d6c17051f
HarDevSan <hardevsandhu@hotmail.de>
2020-09-04 20:14:48 +0200

    remove disabling ojects on collect from InventoryItem script

--- Assets/_Scripts/Inventory Management/InventoryItem.cs
+++ Assets/_Scripts/Inventory Management/InventoryItem.cs
@@ -19,16 +19,30 @@ public class InventoryItem : MonoBehaviour
 
     public float blendInOutYouHaveCollectedxHintSpeed;
 
+    public delegate void ItemHasBeenCollected();
+    public static event ItemHasBeenCollected OnItemHasBeenCollected;
+    public delegate void BlendOutRoutineFinished();
+    public static event BlendOutRoutineFinished OnBlendOutRoutineFinished;
+
+    public static bool isItemCollected;
+
     private void Awake()
     {
-        youHaveCollectedxGroup.alpha = 0;
-   
+        youHaveCollectedxGroup.alpha = 0f;
+       OnItemHasBeenCollected += ChangeMaskToDefault;
     }
-  
+    //Change the layer before the gameobject gets deactivated, so the player cant interact again when he chose to collect the item
+    void ChangeMaskToDefault()
+    {
+        gameObject.layer = LayerMask.NameToLayer("Default");    
+    }
+
     public void AddItemToInventory()
     {
         inventorySO.AddItemToList(itemName);
         inventoryObject.SetActive(false);
+        OnItemHasBeenCollected.Invoke();
+        isItemCollected = true;
         BlendInYouHaveCollectedTxt();
     }
     void BlendInYouHaveCollectedTxt()
@@ -40,11 +54,12 @@ public class InventoryItem : MonoBehaviour
     void BlendOutYouHaveCollectedTxt()
     {
         StartCoroutine(ShowYouHaveCollectedItemblendOutRoutine());
+
     }
 
     IEnumerator ShowYouHaveCollectedItemblendInRoutine()
     {
-        while (youHaveCollectedxGroup.alpha < 0.999)
+        while (youHaveCollectedxGroup.alpha < .999f)
         {
             youHaveCollectedxGroup.alpha = Mathf.Lerp(youHaveCollectedxGroup.alpha, 1, blendInOutYouHaveCollectedxHintSpeed * Time.deltaTime);
             yield return null;
@@ -54,12 +69,13 @@ public class InventoryItem : MonoBehaviour
     }
     IEnumerator ShowYouHaveCollectedItemblendOutRoutine()
     {
-
-        while (youHaveCollectedxGroup.alpha > 0.001)
+        while (youHaveCollectedxGroup.alpha >= 0.015f)
         {
             youHaveCollectedxGroup.alpha = Mathf.Lerp(youHaveCollectedxGroup.alpha, 0, blendInOutYouHaveCollectedxHintSpeed * Time.deltaTime);
             yield return null;
         }
+        if(OnBlendOutRoutineFinished != null)
+        OnBlendOutRoutineFinished.Invoke();
     }
 
 
