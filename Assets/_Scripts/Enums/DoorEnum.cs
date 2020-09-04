00e4fe87039aa0b6c48ac008bfd61b79145c54bf
HarDevSan <hardevsandhu@hotmail.de>
2020-09-04 20:22:27 +0200

    add door types enums

---
+++ Assets/_Scripts/Enums/DoorEnum.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorEnum : MonoBehaviour
+{
+
+    public enum DoorTypesEnum
+    {
+        Heavy,
+        Lightweight
+    }
+
+    public DoorTypesEnum doorEnum;
+}
