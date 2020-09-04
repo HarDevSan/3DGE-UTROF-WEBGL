f846543821c316d750a1cf54d90e7ab60576b52b
HarDevSan <hardevsandhu@hotmail.de>
2020-09-04 20:15:20 +0200

    remove diabling object from ItemActivator Script

--- Assets/_Scripts/Inventory Management/ItemActivator.cs
+++ Assets/_Scripts/Inventory Management/ItemActivator.cs
@@ -4,5 +4,35 @@ using UnityEngine;
 
 public class ItemActivator : SceneTransitionActivator
 {
-    
+
+    private void Awake()
+    {
+        InventoryItem.OnItemHasBeenCollected += ChangeMaskToDefault;
+        //TextEvent_withCollectionPrompt.OnButtonsAreBlendedOutAfterPlayerChoseYes += DisableGameObject;
+    }
+
+    void ChangeMaskToDefault()
+    {
+        gameObject.layer = LayerMask.NameToLayer("Default");
+    }
+
+ 
+
+    //More harm than, it does not need to be disabled, chaning layermask suffices
+    //void DisableGameObject()
+    //{
+    //    gameObject.SetActive(false);
+    //}
+
+    public override void OnTriggerExit(Collider other)
+    {
+        base.OnTriggerExit(other);
+        //if (InventoryItem.isItemCollected)
+        //   // DisableGameObject();
+    }
+
+    private void OnDisable()
+    {
+        InventoryItem.OnItemHasBeenCollected -= ChangeMaskToDefault;
+    }
 }
