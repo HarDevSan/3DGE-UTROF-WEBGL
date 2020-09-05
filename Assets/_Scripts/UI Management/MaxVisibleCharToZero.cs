b17235987c434d1aac339d6d62ac73f4718bbe9b
HarDevSan <hardevsandhu@hotmail.de>
2020-09-05 23:13:19 +0200

    refactor and comment

--- Assets/_Scripts/UI Management/MaxVisibleCharToZero.cs
+++ Assets/_Scripts/UI Management/MaxVisibleCharToZero.cs
@@ -3,22 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
+/*This class carries a list of txts. The list of text have their maximum visible chars reset when the game starts. 
+ * It is filled in the inspector manually per hand. the alternative to get all txt via GetComponent in Awake or Start
+ * and zero out their maximum length is not feasible, as it also zeroes out any txt which should be visible at all times,
+ * such as pause menu, or button content*/
 public class MaxVisibleCharToZero : MonoBehaviour
 {
     public TextMeshProUGUI[] textsToResetMaxVisCharToZeroOnStart;
-    //public CanvasGroup[] textsToZeroOutAlpha;
 
-    // Start is called before the first frame update
     private void Awake()
     {
-        //textsToZeroOutAlpha = FindObjectsOfType<TextMeshProUGUI>();
         foreach (TextMeshProUGUI txt in textsToResetMaxVisCharToZeroOnStart)
         {
             txt.maxVisibleCharacters = 0;
         }
-        //foreach (CanvasGroup txt in textsToZeroOutAlpha)
-        //{
-        //    txt.alpha = 0;
-        //}
+     
     }
 }
