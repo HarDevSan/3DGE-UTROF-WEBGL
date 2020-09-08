de65462b42c130f5671e3f49497dffe328bb1222
HarDevSan <hardevsandhu@hotmail.de>
2020-09-08 22:44:03 +0200

    remove double subscription in Awake

--- Assets/_Scripts/Scene and Persistence Handling/SceneLoader.cs
+++ Assets/_Scripts/Scene and Persistence Handling/SceneLoader.cs
@@ -7,6 +7,7 @@ using UnityEngine.ResourceManagement.AsyncOperations;
 using UnityEngine.ResourceManagement.ResourceProviders;
 using System;
 using UnityEngine.UI;
+using Cinemachine;
 
 public class SceneLoader : MonoBehaviour
 {
@@ -25,10 +26,13 @@ public class SceneLoader : MonoBehaviour
 
     [SerializeField]
     string lastSceneName;
+    public CinemachineBrain brain;
 
     private void Awake()
     {
         //-- Dont forget to UNSUBSCRIBE
+        //This makes no sense, why is it already subscribed here ?
+        SceneTransition.OnPlayerPressedEnterOnSight -= LoadNextScene;
         SceneTransition.OnPlayerPressedEnterOnSight += LoadNextScene;
         SceneTransition.OnPlayerPressedEnterOnSight += UnloadLastScene;
         OnScene_Has_Loaded += PlayerController.SetPlayerToPlayableState;
@@ -36,7 +40,17 @@ public class SceneLoader : MonoBehaviour
 
     private void Start()
     {
-        lastSceneName = "1stFloor_Reception";
+        /*There will never be more than 2 scenes in loaded at once. The Persistent scene an one additional scene.
+         * Cause of this, we can check which scene is loaded at the start of the game
+         * that is NOT named Persistent and then assign its name to the lastSceneName variable.*/
+        Scene[] sceneArray =  SceneManager.GetAllScenes();
+        foreach(Scene scene in sceneArray)
+        {
+            if (!scene.name.Equals("Persistent"))
+            {
+                lastSceneName = scene.name;
+            }
+        }
     }
 
     bool CheckIfSceneHasOutdoors()
@@ -54,19 +68,18 @@ public class SceneLoader : MonoBehaviour
     //-----------Loading
     public void LoadNextScene(string name)
     {
+        Debug.Log("Scene Name param : " + name);
+
         //Start loading of next scene if the next scene is not already loaded
         if (SceneManager.GetSceneByName(name).isLoaded == false)
         {
-            OnSceneStartedLoading.Invoke();
             StartCoroutine(WaitForSceneToFinishLoading(name));
-            //check if the scene that needs to unloaded is loaded alread, if not, spare this step  
         }
     }
 
     //---------Unloading
     void UnloadLastScene(string notNeeded)
     {
-        Debug.Log("UNLOAD CALLED");
         //Only unload a scene if it's loaded. Otherwise spare the coroutine
         if (SceneManager.GetSceneByName(lastSceneName).isLoaded == true)
         {
@@ -79,6 +92,9 @@ public class SceneLoader : MonoBehaviour
     //----------CoRoutines, managing loading progress and finish 
     IEnumerator WaitForSceneToFinishLoading(string name)
     {
+        OnSceneStartedLoading.Invoke();
+
+        brain.enabled = false;
         //Loading the nextScene and thereby create an async operation
         AsyncOperation operation = SceneManager.LoadSceneAsync(name, LoadSceneMode.Additive);
         //Check if the Async Operation has already been created before doing anything, prevents nullref 
@@ -87,46 +103,45 @@ public class SceneLoader : MonoBehaviour
 
             while (operation.isDone == false)
             {
-                //Debug.Log("LoadingInProgress");
                 OnSceneIsLoading.Invoke();
                 loadingprogress = operation.progress;
                 yield return null;
             }
         }
-        //Debug.Log("Loading Finished");
         loadingprogress = 0;
         lastSceneName = name;
         yield return new WaitForSeconds(loadDelay);
 
         OnScene_Has_Loaded.Invoke();
-        //Debug.Log("OnScene_Has_Loaded.Invoke");
-        // Debug.Log("Scene that will unload next is : " + lastSceneName);
+        brain.enabled = true;
+
     }
 
     IEnumerator WaitForSceneToFinishUnloading()
     {
         //unLoading the thisScene and thereby create an async operation
         AsyncOperation op = SceneManager.UnloadSceneAsync(lastSceneName);
-        //Debug.Log("Name Of the LAst Scene : " + lastSceneName);
         if (op != null)
         {
             while (op.isDone == false)
             {
-                //Debug.Log("IsUnloading");
+                Debug.Log("IsUnloading");
                 yield return null;
             }
 
 
         }
-        // Debug.Log("finishedUnloading ");
         OnScene_Has_UnLoaded.Invoke();
-        //Debug.Log("OnSceneHasUnloaded was invoked");
-        yield break;
     }
     private void OnDisable()
     {
         SceneTransition.OnPlayerPressedEnterOnSight -= LoadNextScene;
         SceneTransition.OnPlayerPressedEnterOnSight -= UnloadLastScene;
     }
+    //private void OnEnable()
+    //{
+    //    SceneTransition.OnPlayerPressedEnterOnSight += LoadNextScene;
+    //    SceneTransition.OnPlayerPressedEnterOnSight += UnloadLastScene;
+    //}
 }
 
