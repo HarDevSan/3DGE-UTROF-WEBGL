6a0fdec208c7bdbbd1e4ddf2b3708d228e5c0c9f
HarDevSan <hardevsandhu@hotmail.de>
2020-09-08 22:38:50 +0200

    add Quit prompt shower scripts

---
+++ Assets/_Scripts/UI Management/UI_QuitPromptShower.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_QuitPromptShower : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public CanvasGroup quitPRomptGroup;
+
+    private void Awake()
+    {
+        ReadNote.OnPlayerHasChosenNo += HideQuitPromt;
+    }
+
+    public void ShowQuitPrompt()
+    {
+        quitPRomptGroup.alpha = 1;
+    }
+    public void HideQuitPromt()
+    {
+        quitPRomptGroup.alpha = 0;
+
+    }
+}
