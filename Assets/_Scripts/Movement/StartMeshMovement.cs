4dcc333506f9cfeba9e93417d1928dda459ccbd6
HarDevSan <hardevsandhu@hotmail.de>
2020-09-16 23:43:15 +0200

    add movement folder and script

---
+++ Assets/_Scripts/Movement/StartMeshMovement.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartMeshMovement : MonoBehaviour
+{
+    public Animator animator;
+    public string triggerName;
+
+    public void StartMovement()
+    {
+        animator.SetTrigger(triggerName);
+    }
+}
