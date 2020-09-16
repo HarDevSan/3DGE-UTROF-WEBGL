16bc363842b5977069aa2d90767d73265af548eb
HarDevSan <hardevsandhu@hotmail.de>
2020-09-16 23:43:40 +0200

    add break light script to vfx

---
+++ Assets/_Scripts/Visual Effects/BreakLight.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakLight : MonoBehaviour
+{
+    public Light light;
+    public LightBehaviour lightBehaviour;
+    public float maxFlickerSpeed;
+    public float flickerIncreaseSpeed;
+    public AudioSource lightBreakingSound;
+
+    private void Start()
+    {
+        //Setthe light intensity whie flickering to the lights intensity in the scene
+        lightBehaviour.flickerLightIntensity = light.intensity;
+    }
+
+    public void StartBreakLight()
+    {
+        StartCoroutine(BreakLightsRoutine());
+    }
+
+    IEnumerator BreakLightsRoutine()
+    {
+
+
+        float t = 0f;
+        float toLerpFrom = 0f;
+        float toLerpTo = maxFlickerSpeed;
+        float lerpValue = 0f;
+
+        while (lerpValue < toLerpTo)
+        {
+            t += flickerIncreaseSpeed * Time.deltaTime;
+            lerpValue = Mathf.Lerp(toLerpFrom, toLerpTo, t);
+            lightBehaviour.flickerSpeed = lerpValue;
+            yield return null;
+        }
+        lightBreakingSound.Play();
+        light.intensity = 0f;
+    }
+
+   
+
+
+}
