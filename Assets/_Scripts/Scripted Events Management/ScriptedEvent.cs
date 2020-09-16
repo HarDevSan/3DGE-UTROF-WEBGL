424b64b7776667908c7feae6a5032b965cfd2078
HarDevSan <hardevsandhu@hotmail.de>
2020-09-16 15:09:41 +0200

    add scripted events script

---
+++ Assets/_Scripts/Scripted Events Management/ScriptedEvent.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptedEvent : MonoBehaviour
+{
+
+    [Header("AudioSources")]
+    public AudioSource soundToPlay;
+
+    bool isSoundAlreadyPlayed;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(isSoundAlreadyPlayed == false) 
+        soundToPlay.Play();
+
+        isSoundAlreadyPlayed = true;
+    }
+}
