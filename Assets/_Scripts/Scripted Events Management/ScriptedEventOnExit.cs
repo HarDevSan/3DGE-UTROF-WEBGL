28b8d5db89bf3c267856e7c090976ecc7058c98b
HarDevSan <hardevsandhu@hotmail.de>
2020-09-22 22:11:59 +0200

    add scripted event for  trigger exit

---
+++ Assets/_Scripts/Scripted Events Management/ScriptedEventOnExit.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptedEventOnExit : ScriptedEvent
+{
+
+    public override void OnTriggerEnter(Collider other)
+    {
+        //Do nothing in this case
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        InvokeScriptedEvent();
+    }
+}
