376d85348031f511e8462e37f95340110aff27b6
HarDevSan <hardevsandhu@hotmail.de>
2020-10-05 23:51:00 +0200

    add lighter script

---
+++ Assets/_Scripts/Visual Effects/Lighter.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lighter : MonoBehaviour
+{
+    public Light lighter;
+
+    private void Awake()
+    {
+        InputReceiver.On_F_Inpu += EnableLighter;
+        InputReceiver.On_F_Second_Input += DisableLighter;
+    }
+
+    void EnableLighter()
+    {
+        Debug.Log("FunctionReached");
+        lighter.enabled = true;
+    }
+    void DisableLighter()
+    {
+        lighter.enabled = false;
+    }
+
+
+    private void OnEnable()
+    {
+        InputReceiver.On_F_Inpu += EnableLighter;
+        InputReceiver.On_F_Second_Input += DisableLighter;
+    }
+    private void OnDisable()
+    {
+        InputReceiver.On_F_Inpu -= EnableLighter;
+        InputReceiver.On_F_Second_Input -= DisableLighter;
+    }
+}
