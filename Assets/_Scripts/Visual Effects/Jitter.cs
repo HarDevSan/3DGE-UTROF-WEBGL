f0f415aa6965b84f6856c9223764998d41db2b43
HarDevSan <hardevsandhu@hotmail.de>
2020-10-05 23:50:39 +0200

    add jitter effect for lighter

---
+++ Assets/_Scripts/Visual Effects/Jitter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Jitter : MonoBehaviour
+{
+    public Light light;
+    [Range(1,3)]
+    public float lightRandomangeMin;
+    [Range(2, 4)]
+    public float lightRandomangeMax; 
+
+    // Update is called once per frame
+    void Update()
+    {
+        light.intensity = Random.Range(lightRandomangeMin, lightRandomangeMax);
+    }
+}
