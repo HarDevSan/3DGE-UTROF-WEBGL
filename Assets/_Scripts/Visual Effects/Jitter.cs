559700a701415fdbb8941d8132c8b51550120e73
HarDevSan <hardevsandhu@hotmail.de>
2020-10-06 22:30:08 +0200

    add jitter to light transform

--- Assets/_Scripts/Visual Effects/Jitter.cs
+++ Assets/_Scripts/Visual Effects/Jitter.cs
@@ -9,11 +9,35 @@ public class Jitter : MonoBehaviour
     [Range(1,3)]
     public float lightRandomangeMin;
     [Range(2, 4)]
-    public float lightRandomangeMax; 
+    public float lightRandomangeMax;
+
+    public Vector3 lightDefaultPos;
+    public Vector3 randomRangeAdd;
+
+    private void Start()
+    {
+        lightDefaultPos = light.transform.localPosition;
+    }
+
 
     // Update is called once per frame
     void Update()
     {
+        RandomizeLightRange();
+        RandomizeLightPos();
+    }
+
+    void RandomizeLightPos()
+    {
+        //ADD value on top of the maximum random value
+        light.transform.localPosition = new Vector3(Random.Range(lightDefaultPos.x, lightDefaultPos.x + randomRangeAdd.x),
+                                            Random.Range(lightDefaultPos.y, lightDefaultPos.y +  randomRangeAdd.y),
+                                            Random.Range(lightDefaultPos.z, lightDefaultPos.z +  randomRangeAdd.z));
+    }
+
+    void RandomizeLightRange()
+    {
         light.intensity = Random.Range(lightRandomangeMin, lightRandomangeMax);
+
     }
 }
