81c174743ad4d0e456622c5209fc6d99614d8b3f
HarDevSan <hardevsandhu@hotmail.de>
2020-10-11 22:39:58 +0200

    add new skyBoxRotator Script

---
+++ Assets/_Scripts/Visual Effects/SkyBoxRotator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyBoxRotator : MonoBehaviour
+{
+
+    //public Material skyBoxMat;
+    Transform skyBoxTransform;
+    public float rotSpeedX;
+    public float rotSpeedY;
+    public float rotSpeedZ;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        //skyBox = RenderSettings.skybox;
+        skyBoxTransform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        skyBoxTransform.Rotate(new Vector3(rotSpeedX, rotSpeedY, rotSpeedZ));
+        //skyBoxMat.SetFloat("_Rotation", Time.time * rotSpeed);
+    }
+}
+
