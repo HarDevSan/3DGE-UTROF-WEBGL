5f5b958f311ef15fcb4011544a7cc944ae796628
HarDevSan <hardevsandhu@hotmail.de>
2020-10-11 22:40:46 +0200

    add thunderstorm script to vfx

---
+++ Assets/_Scripts/Visual Effects/ThunderStorm.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThunderStorm : MonoBehaviour
+{
+    Material material;
+    Color materialEmissiveColor;
+    Color thunderEmissiveColor;
+
+    private void Start()
+    {
+        material = GetComponent<Material>();
+        materialEmissiveColor = material.GetColor("_EmissionColor");
+    }
+
+    private void Update()
+    {
+        Color tempCol = new Color();
+        float thunder = Mathf.PingPong(Time.time, thunderEmissiveColor.a);
+        tempCol.a = thunder;
+        material.SetColor("_EmissionColor", tempCol); 
+    }
+}
