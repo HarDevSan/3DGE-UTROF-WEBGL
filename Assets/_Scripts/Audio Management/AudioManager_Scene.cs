aec25242a6d1c8170b39d173f5844e39fe112efd
HarDevSan <hardevsandhu@hotmail.de>
2021-03-04 18:22:41 +0100

    add audiomanager class to manage Awake behaviour of non persistent audio sources

---
+++ Assets/_Scripts/Audio Management/AudioManager_Scene.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager_Scene : MonoBehaviour
+{
+    //List of per Scene audio clips
+    public List<AudioSource> audioSources_BathRoom = new List<AudioSource>();
+
+    //temporary List to pass on
+
+    string currentSceneName = "";
+    string lastSceneName = "";
+
+    private void Awake()
+    {
+        SceneLoader.OnScene_Has_Loaded += PlayAudioSourcesBelongingtoThisScene;    }
+
+    void GetCurrentSceneName()
+    {
+        currentSceneName = SceneLoader.thisSceneName;
+    }
+
+    void GetLastSceneName()
+    {
+        lastSceneName = SceneLoader.lastSceneName;
+
+    }
+
+    void PlayAudioSourcesBelongingtoThisScene()
+    {
+        GetCurrentSceneName();
+
+        switch (currentSceneName)
+        {
+            case "1stFloor_Reception RestRoom_F":
+                PlayAllAudioSourcesOfScene_Bathroom();
+                break;
+            default:
+                StopAllAudioSources();
+                break;
+        }
+    }
+
+
+    void PlayAllAudioSourcesOfScene_Bathroom()
+    {
+        foreach (AudioSource src in audioSources_BathRoom)
+        {
+            src.Play();
+        }
+    }
+    void StopAllAudioSources()
+    {
+        foreach (AudioSource src in audioSources_BathRoom)
+        {
+            src.Stop();
+        }
+    }
+
+
+
+}
