860f5a8f02de2a66f55d283a4fa5e9036fe439ad
HarDevSan <hardevsandhu@hotmail.de>
2021-03-04 22:12:08 +0100

    add AudioManager_TimeLine to manage playback of mutliple event driven PlayableDirectors per scene

---
+++ Assets/_Scripts/Audio Management/AudioManager_TimeLine.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class AudioManager_TimeLine : MonoBehaviour
+{
+    [Header("initialtimeLineBathroom")]
+    public PlayableDirector firstTimeline_BathRoom;
+
+    //List of all non persistent Scene timelines to cycle through
+    public List<PlayableDirector> nonPersistetnTimelineList = new List<PlayableDirector>();
+
+
+    string currentSceneName = "";
+
+    private void Awake()
+    {
+        SceneLoader.OnScene_Has_Loaded += PlayFirstTimelineBelongingtoThisScene;
+    }
+
+    void GetCurrentSceneName()
+    {
+        currentSceneName = SceneLoader.thisSceneName;
+    }
+
+
+    void PlayFirstTimelineBelongingtoThisScene()
+    {
+        GetCurrentSceneName();
+
+        switch (currentSceneName)
+        {
+            case "1stFloor_Reception RestRoom_F":
+                PlayTimeLine_Bathroom();
+                break;
+            default:
+                StopAllTimeLines();
+                break;
+        }
+    }
+
+
+    void PlayTimeLine_Bathroom()
+    {
+    
+            firstTimeline_BathRoom.Play();
+        
+    }
+    void StopAllTimeLines()
+    {
+        foreach (PlayableDirector pd in nonPersistetnTimelineList)
+        {
+            pd.Stop();
+        }
+
+    }
+}
