178ec3ed7b6d9e32c8a9d6865e74d1ab0f5274a4
HarDevSan <hardevsandhu@hotmail.de>
2021-04-11 09:26:25 +0200

    refactor AudioManager_Scene

--- Assets/_Scripts/Audio Management/AudioManager_Scene.cs
+++ Assets/_Scripts/Audio Management/AudioManager_Scene.cs
@@ -2,6 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/* This class playbacks the audiosources that belong to a scene. E.g.: When a bathroom is loaded, it plays back the bathroom ambience audio sources.
+ * This class is needed because Audio Sources that need dynmic behaviour such as play, pause or stop during gameplay need to live in the persistent scene, as 
+ * cross referenes between scenes are not possible. Therefore, to able to control which persistent audio source gets played when, based onthe players interactions, 
+ * this class uses a method with a switch case on the next scene's name string to play the persistent audio sources that should only play AFTER a new scene is entered.
+ * */
 public class AudioManager_Scene : MonoBehaviour
 {
     //List of per Scene audio clips
@@ -14,7 +19,9 @@ public class AudioManager_Scene : MonoBehaviour
 
     private void Awake()
     {
-        SceneLoader.OnScene_Has_Loaded += PlayAudioSourcesBelongingtoThisScene;    }
+        SceneLoader.OnScene_Has_Loaded += PlayAudioSourcesBelongingtoThisScene;    
+    
+    }
 
     void GetCurrentSceneName()
     {
@@ -50,6 +57,13 @@ public class AudioManager_Scene : MonoBehaviour
             src.Play();
         }
     }
+
+    void StopLastSceneSources()
+    {
+        GetLastSceneName();
+
+    }
+
     void StopAllAudioSources()
     {
         foreach (AudioSource src in audioSources_BathRoom)
