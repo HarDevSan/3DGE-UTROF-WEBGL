33c37e8661df1af7c94d03cb9083bf7310b32220
HarDevSan <hardevsandhu@hotmail.de>
2021-06-30 12:05:21 +0200

    update fps shower to properly display miliseconds

--- Assets/_Scripts/Utilities/FPSshower.cs
+++ Assets/_Scripts/Utilities/FPSshower.cs
@@ -19,40 +19,57 @@ public class FPSshower : MonoBehaviour
     public Color greaterEqual30Below60_Color;
     public Color below30_Color;
 
+    int counter;
+
 
     // Update is called once per frame
     void Update()
     {
 
-        //Calculate framerate and cast to int
-        fps = (int)(1f / Time.smoothDeltaTime);
-        fpsCounter.text = "FPS: " + fps;
-        //Calculate miliseconds and cast to float
-        miliSeconds.text = "MS: " + (float)1 / fps;
-        miliSeconds.maxVisibleCharacters = 9;
+            //Counter for the displaying interval
+            counter++;
+        
 
-        //Color coding depending on fps range
-        if (fps >= 59)
-        {
-            fpsCounter.color = greaterEqual60_Color;
-            miliSeconds.color = greaterEqual60_Color;
+        Debug.Log(counter);
 
-        }
-        else if (fps < 60 && fps >= 30)
+        //display only every 10th frame
+        if (counter == 10)
         {
-            fpsCounter.color = greaterEqual30Below60_Color;
-            miliSeconds.color = greaterEqual30Below60_Color;
-        }
-        else if (fps < 30)
-        {
-            fpsCounter.color = below30_Color;
-            miliSeconds.color = below30_Color;
-        }
-        else
-        {
-            fpsCounter.color = below30_Color;
-            miliSeconds.color = below30_Color;
-        }
 
+            //Calculate framerate and cast to int
+            fps = (int)(1f / Time.smoothDeltaTime);
+            fpsCounter.text = "FPS: " + fps;
+            //Calculate miliseconds and cast to float
+            miliSeconds.text = "MS: " + ((float)1 / fps) * 1000;
+            miliSeconds.maxVisibleCharacters = 9;
+
+            //Color coding depending on fps range
+            if (fps >= 59)
+            {
+                fpsCounter.color = greaterEqual60_Color;
+                miliSeconds.color = greaterEqual60_Color;
+
+            }
+            else if (fps < 60 && fps >= 30)
+            {
+                fpsCounter.color = greaterEqual30Below60_Color;
+                miliSeconds.color = greaterEqual30Below60_Color;
+            }
+            else if (fps < 30)
+            {
+                fpsCounter.color = below30_Color;
+                miliSeconds.color = below30_Color;
+            }
+            else
+            {
+                fpsCounter.color = below30_Color;
+                miliSeconds.color = below30_Color;
+            }
+            //reset counter
+            counter = 0;
+        }
+        
     }
+
 }
+
