e730062886f4a208a4c292969ffa181a05f0e935
HarDevSan <hardevsandhu@hotmail.de>
2021-07-06 20:43:49 +0200

    add camera zoom script

---
+++ Assets/_Scripts/Camera Management/CameraZoom.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraZoom : MonoBehaviour
+{
+    public CinemachineFreeLook zoomCam;
+    public CinemachineVirtualCamera zoomCamFPS;
+
+
+    private void Awake()
+    {
+        InputReceiver.On_Z_Input += RaiseZoomCamPrio;
+        InputReceiver.On_Z_Input_Up += LowerZoomCamPrio;
+    }
+
+    void RaiseZoomCamPrio()
+    {
+        zoomCamFPS.Priority = zoomCamFPS.m_Priority +1;
+    }
+    void LowerZoomCamPrio()
+    {
+        zoomCamFPS.Priority = zoomCamFPS.m_Priority -1;
+    }
+}
