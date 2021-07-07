9b08eb26944edc52030409adce6e1dc065ce1257
HarDevSan <hardevsandhu@hotmail.de>
2021-07-08 00:49:30 +0200

    add act-deactivate supervisor UI elements script

---
+++ Assets/_Scripts/SupervisorExploration/Act_DeactivteUIElementOnEnter_Exit.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Act_DeactivteUIElementOnEnter_Exit : MonoBehaviour
+{
+    public GameObject gameObjectToActivate;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+            gameObjectToActivate.SetActive(true);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+            gameObjectToActivate.SetActive(false);
+    }
+
+}
