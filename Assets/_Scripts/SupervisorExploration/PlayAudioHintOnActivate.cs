2ce9a8565248accf4738e402e42e5d55cd13890c
HarDevSan <hardevsandhu@hotmail.de>
2021-07-08 00:50:56 +0200

    add audio hint functionality for supervisor ui notifications

---
+++ Assets/_Scripts/SupervisorExploration/PlayAudioHintOnActivate.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAudioHintOnActivate : MonoBehaviour
+{
+    AudioSource audioSource;
+    public AudioClip clipEnter;
+    public AudioClip clipExit;
+
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        audioSource.clip = clipEnter;
+        audioSource.Play();
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        audioSource.clip = clipExit;
+        audioSource.Play();
+    }
+}
