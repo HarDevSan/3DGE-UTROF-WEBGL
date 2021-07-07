3cb9f5c3b911808d2ac0ecc5b3dcf8da4eb74967
HarDevSan <hardevsandhu@hotmail.de>
2021-07-08 00:45:22 +0200

    add supervisor text element rotateToCamera script

---
+++ Assets/_Scripts/SupervisorExploration/RotateToCamera.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//This class rotates an UI element towards the main cameras forward vector only for the time the player stays inside this trigger
+public class RotateToCamera : MonoBehaviour
+{
+
+    Transform mainCamTrans;
+    [Range(.01f, 3)]
+    public float rotSpeed;
+
+    public Transform toTransform;
+    float initialX;
+
+    void Start()
+    {
+        mainCamTrans = Camera.main.transform;
+    }
+
+    //Only calculate roation when player inside trigger. Prevents use of Update.
+    private void OnTriggerStay(Collider other)
+    {
+
+        initialX = toTransform.rotation.eulerAngles.x;
+        Quaternion newRot = new Quaternion();
+
+
+        if (other.tag == "Player")
+        {
+
+            newRot = Quaternion.Slerp(toTransform.rotation, Quaternion.LookRotation(mainCamTrans.forward), Time.deltaTime * rotSpeed);
+            toTransform.rotation = newRot;
+            toTransform.eulerAngles = new Vector3(initialX, toTransform.eulerAngles.y, 0);
+        }
+    }
+
+}
