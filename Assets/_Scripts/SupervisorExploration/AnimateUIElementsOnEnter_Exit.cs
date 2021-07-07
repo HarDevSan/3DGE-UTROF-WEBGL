4832884d73f39a6e51ff96c1f2c35576fbb0ddda
HarDevSan <hardevsandhu@hotmail.de>
2021-07-08 00:52:16 +0200

    add script to play supervisor ui build up animation on poi enter

---
+++ Assets/_Scripts/SupervisorExploration/AnimateUIElementsOnEnter_Exit.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimateUIElementsOnEnter_Exit : MonoBehaviour
+{
+    Animator animator;
+
+    public delegate void ReverseAnimCompleted();
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+            animator.Play("ExpandPOI");
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+            animator.Play("Default");
+    }
+
+}
