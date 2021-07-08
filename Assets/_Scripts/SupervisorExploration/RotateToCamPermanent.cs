281c50dba377ea34f7defc1054e56b37c1db3e7a
HarDevSan <hardevsandhu@hotmail.de>
2021-07-08 11:32:20 +0200

    add script for permanent cam facing rotation for UI lements

---
+++ Assets/_Scripts/SupervisorExploration/RotateToCamPermanent.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateToCamPermanent : MonoBehaviour
+{
+    Transform mainCamTrans;
+    [Range(.01f, 3)]
+    public float rotSpeed;
+
+    public Transform toTransform;
+    float initialX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamTrans = Camera.main.transform;
+        initialX = toTransform.rotation.eulerAngles.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Quaternion newRot = new Quaternion();
+
+        newRot = Quaternion.Slerp(toTransform.rotation, Quaternion.LookRotation(mainCamTrans.forward), Time.deltaTime * rotSpeed);
+        toTransform.rotation = newRot;
+        toTransform.eulerAngles = new Vector3(initialX, toTransform.eulerAngles.y, 0);
+
+    }
+}
