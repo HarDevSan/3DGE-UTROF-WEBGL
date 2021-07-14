5139703b92ef822bb0cb841012cc7e92de987929
HarDevSan <hardevsandhu@hotmail.de>
2021-07-15 00:20:21 +0200

    add generic timeline on interaction player script

---
+++ Assets/_Scripts/Interactivity/PlayTimeLineOnInteraction.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Timeline;
+using UnityEngine.Playables;
+
+public class PlayTimeLineOnInteraction : MonoBehaviour
+{
+    public PlayableDirector timeLineToPlay;
+
+    private void Start()
+    {       
+        InputReceiver.On_E_Input += PlayTimeLine;
+    }
+
+
+    void PlayTimeLine()
+    {
+        timeLineToPlay.Play();
+    }
+
+    private void OnDisable()
+    {
+        InputReceiver.On_E_Input -= PlayTimeLine;
+
+    }
+}
