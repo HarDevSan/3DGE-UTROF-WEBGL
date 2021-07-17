e63d0f4a28ef98d032b3b0cfab08e846e3dc4aa6
HarDevSan <hardevsandhu@hotmail.de>
2021-07-17 23:55:37 +0200

    comment persistent handler class

--- Assets/_Scripts/Scripted Events Management/PersistenceHandler.cs
+++ Assets/_Scripts/Scripted Events Management/PersistenceHandler.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//This class should handle object and gameplay states and stats between scenes.
 public class PersistenceHandler : MonoBehaviour
 {
-    bool isSHadowMenAlreadAppered;
+    //Example could be that the player could have a hallucinations only once, and when he returns form the next scene,this halucination should have disappeared.
+    public static bool isSHadowMenAlreadAppered;
 }
