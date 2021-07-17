d15285fd074b78eba45040656bcfc555c152595a
HarDevSan <hardevsandhu@hotmail.de>
2021-07-17 23:56:11 +0200

    refactor scriptedEventOnExit-now obsolete

--- Assets/_Scripts/Scripted Events Management/ScriptedEventOnExit.cs
+++ Assets/_Scripts/Scripted Events Management/ScriptedEventOnExit.cs
@@ -2,16 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ScriptedEventOnExit : ScriptedEvent
+public class ScriptedEventOnExit : MonoBehaviour
 {
 
-    public override void OnTriggerEnter(Collider other)
+    public void OnTriggerEnter(Collider other)
     {
         //Do nothing in this case
     }
 
     private void OnTriggerExit(Collider other)
     {
-        InvokeScriptedEvent();
     }
 }
