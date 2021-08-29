a878f8bf557a30a82197dc1a8f5f7b6f0b5004be
HarDevSan <hardevsandhu@hotmail.de>
2021-08-29 16:28:42 +0200

    add new interactivity scripts

---
+++ Assets/_Scripts/Interactivity/InteractableObjectActivator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableObjectActivator : MonoBehaviour
+{
+    public GameObject hierarchyToActivate;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        hierarchyToActivate.SetActive(true);
+    }
+}
---
+++ Assets/_Scripts/Interactivity/OpenDoor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class OpenDoor: MonoBehaviour
+{
+    public Animator doorAnimator;
+
+    private void Start()
+    {
+        InputReceiver.On_E_Input += PlayDoorAnim;
+    }
+
+
+    void PlayDoorAnim()
+    {
+        
+            doorAnimator.SetTrigger("doorOpened");
+        
+    }
+
+    
+}
