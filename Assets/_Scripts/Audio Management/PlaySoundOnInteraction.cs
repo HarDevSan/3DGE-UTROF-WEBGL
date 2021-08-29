388932d39d354373c4bd0e9e39fe67550beaa7c8
HarDevSan <hardevsandhu@hotmail.de>
2021-08-29 16:28:04 +0200

    add new audio management scripts

---
+++ Assets/_Scripts/Audio Management/PlaySoundOnInteraction.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySoundOnInteraction : MonoBehaviour
+{
+    public Animator audioAnimator;
+
+    private void Start()
+    {
+        InputReceiver.On_E_Input += PlayAudioSources;
+    }
+
+
+    void PlayAudioSources()
+    {
+
+    }
+}
