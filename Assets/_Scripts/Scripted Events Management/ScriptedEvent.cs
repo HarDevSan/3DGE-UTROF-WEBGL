bbbba5ba344690e3f6e2c5ecca8afdeaec097945
HarDevSan <hardevsandhu@hotmail.de>
2022-04-16 22:53:14 +0200

    comment corrections

--- Assets/_Scripts/Scripted Events Management/ScriptedEvent.cs
+++ Assets/_Scripts/Scripted Events Management/ScriptedEvent.cs
@@ -5,19 +5,22 @@ using UnityEngine.Events;
 
 public class ScriptedEvent : MonoBehaviour
 {
-    /* Scripted Events are an intgral part of any playable 3d game envrionment. The game design theory is that any element like soundFX or visualFX 
-    * shall occur when the player enters certain areas or spots of interest. It could also be something like a sound effect playing when the player enters a room
-    * or opens a door. It could be that on pressing use on a console, the time of day changes.
-    * The big advantage of using Unity in this context is that any components function can be called inside a dynamic list of functions attached to this event.
-    * It separates the event triggerign from the logic, which makes it highly versatile due to easily exchangable behaviour.
+    /* Scripted Events are an intgral part of any playable 3D game envrionment. The game design theory is that any element like soundFX or visualFX 
+    * shall occur when the player enters certain areas or spots of interest, or interacts. It could be something like a sound effect playing when the player enters a room
+    * or opens a door. It could be that when pressing the "use" key on a console, the time of day changes. The possibilities are and should endless in a flexible Scripted
+    * Event System. The big advantage of using Unity in this context is that any components function can be called inside a dynamic list of functions attached to this
+    * event. It separates the event triggering from the logic, which makes it highly versatile due to easily exchangable and chainable behaviour.
     */
 
-    /*Serialized events in Unity makes this aspect of development acessable, because it propagates the complexity to the editor's hierarchy and inspector, instead of the
-    * need to tightly couple elements through code.This makes it possible to modularly stack up these "Fear emitters". The emitter does not care what calls it. 
-    * Any functioncan be used to trigger the Unit Event. The Unity Event an call any funtions assigned to it. The benefit of this approach is that seemingly complex dynamic behavior and interactions in the game world can be created through minimal coupling. Furthermore it is non destructive through its modular approach, which offers a lot of control at editing time.*/
+    /* Serialized events in Unity makes this aspect of development natively acessable, because it propagates the complexity to the editor's hierarchy and inspector, instead
+    * of the need to tightly couple elements through code.This makes it possible to modularly stack up these "Fear emitters". The emitter does not care what calls it. 
+    * Any functioncan be used to trigger the Unit Event. The Unity Event an call any funtions assigned to it. The benefit of this approach is that seemingly complex dynam 
+    * behavior and interactions in the game world can be created through minimal coupling. Furthermore it is non destructive through its modular approach, which offers a
+    * lot of control at editing time.
+    */
     public UnityEvent scriptedEvent;
 
-    //List of evetns that can be wired to other functiosn to trigger THIS Unity Event
+    //List of events that can be wired to other functions to trigger THIS Unity Event
     public delegate void PlayerPressedUsedStartEmitter();
     public static event PlayerPressedUsedStartEmitter OnPlayerPressedUsedStartEmitter;
 
@@ -40,6 +43,7 @@ public class ScriptedEvent : MonoBehaviour
 
     private void Update()
     {
+        //Look based events need to take place in Update, cause it needs to be known if the player has looked at something.
         if (PlayerController.isPlayerCanInteractBecauseHeLooksAt_ScriptedEvent && isThisEventLookAtBased && isHasSeeOnce == false)
         {
             StartScriptedEvent();
@@ -47,6 +51,7 @@ public class ScriptedEvent : MonoBehaviour
         }
         else
         {
+            //flip flop isHasSeenOnce bool
             if(isHasSeeOnce == true)
             isHasSeeOnce = false;
         }
@@ -55,6 +60,8 @@ public class ScriptedEvent : MonoBehaviour
     void StartScriptedEvent()
     {
         scriptedEvent.Invoke();
+        //Prevents a second trigger of the scripted event was already fired
+       if(shallDeactivateItselfAfterEvent)
         DeactivateThisObject();
     }
 
@@ -68,8 +75,8 @@ public class ScriptedEvent : MonoBehaviour
         gameObject.SetActive(false);
     }
 
-    /* More specific example of using the emitter. It can catch other events to occur in the game world, e.g. the player pressign the use key at a specific location.
-     * than it can trigger Unity event to play whatever other functions or components are attached to it. This could be other function calls or component behaviour like
+    /* More specific example of using the emitter. It can catch other events to occur in the game world, e.g. the player pressing the use key at a specific location.
+     * Then it can trigger Unity event to play whatever other functions or components are attached to it. This could be other function calls or component behaviour like
      * playing an animation clip.
      */
     void StartScriptedEventOnPlayerPressedUse()
@@ -84,7 +91,7 @@ public class ScriptedEvent : MonoBehaviour
 
 
     /* This is the simplest form of execution. On entering the assigned trigger, all assigned functions or animations get fired.
-     * More complex behaviour could be authored when other functions, see functions above. this behavior can be bypassed by simply not addign a trigger to THIS object.
+     * More complex behaviour could be authored when other functions, see functions above. This behavior can be bypassed by simply not adding a trigger to THIS object.
      */
     private void OnTriggerEnter(Collider other)
     {
@@ -110,6 +117,7 @@ public class ScriptedEvent : MonoBehaviour
         {
             isPlayerInsideTrigger = true;
         }
+        //Make sure that once the player already was in the volume, THIS object and hence this collider will be disabled
         DeactivateThisObject();
 
     }
