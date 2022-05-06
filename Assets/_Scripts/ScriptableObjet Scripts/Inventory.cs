3ac2bfb6ee18570621c8ccf2d5407b261ceef89c
HarDevSan <hardevsandhu@hotmail.de>
2022-05-06 13:49:01 +0200

    remove stutter causing iteration

--- Assets/_Scripts/ScriptableObjet Scripts/Inventory.cs
+++ Assets/_Scripts/ScriptableObjet Scripts/Inventory.cs
@@ -8,34 +8,13 @@ using UnityEngine.SceneManagement;
 public class Inventory : ScriptableObject
 {
     public List<string> itemList;
-   
-
-
-    public bool SearchListFor(string name)
-    {
-        for (int i = 0; i < itemList.Count; i++)
-        {
-            if (itemList[i].Equals(name))
-            {
-                return true;
-
-            }
-            else
-            {
-                return false;
-            }
-            
-
-        }
-
-        return false;
-    }
 
     public void AddItemToList(string name)
     {
         Debug.Log("Added item to inventory called : " + name);
         itemList.Add(name);
     }
+
 
 }
 
