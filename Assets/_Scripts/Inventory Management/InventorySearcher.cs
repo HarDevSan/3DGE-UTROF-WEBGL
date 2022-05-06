6b459e47f6cc5fb9ecc126d3759f1e4147c2b87a
HarDevSan <hardevsandhu@hotmail.de>
2022-05-06 14:04:10 +0200

    implement coroutine in new inventorySearcher class

---
+++ Assets/_Scripts/Inventory Management/InventorySearcher.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventorySearcher : MonoBehaviour
+{
+    public Inventory inventory;
+    bool isItemInList;
+
+    public bool CheckIfItemIsInList(string name)
+    {
+        StartCoroutine(CheckIfItemsIsInListRoutine(name));
+        return isItemInList;
+    }
+
+    IEnumerator CheckIfItemsIsInListRoutine(string name)
+    {
+        int counter = 0;
+
+        while (counter < inventory.itemList.Count)
+        {
+            if (inventory.itemList[counter] == name)
+                isItemInList = false;
+            yield return null;
+        }
+        isItemInList = true;
+        yield return null;
+    }
+}
