7c242288c6ed4ce067b778a37297f20105262bb0
HarDevSan <hardevsandhu@hotmail.de>
2022-05-06 14:03:38 +0200

    seperate camera brain disabling from GameManager class

---
+++ Assets/_Scripts/Camera Management/CameraBrainDisabler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraBrainDisabler : MonoBehaviour
+{
+    public CinemachineBrain freeLookCamBrain;
+
+
+    private void Awake()
+    {
+        SceneLoader.OnSceneStartedLoading += TemporaryDisableBrain;
+        SceneLoader.OnScene_Has_Loaded += ReEnableBrain;
+
+    }
+
+
+    void TemporaryDisableBrain()
+    {
+        freeLookCamBrain.enabled = false;
+    }
+        
+    void ReEnableBrain()
+    {
+        freeLookCamBrain.enabled = true;
+
+    }
+
+}
