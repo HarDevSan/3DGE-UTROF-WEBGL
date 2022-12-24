af3e42aac51ba98d1998a2f1d56f54ef5ec2c436
MININT-00KO2OI\Hardev <walter_sullivan@hotmail.de>
2022-12-24 20:34:07 +0100

    add temp script for interaction test

---
+++ Assets/_Scripts/Interactivity/Refactor/FireEventOnUseKey.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+public class FireEventOnUseKey : MonoBehaviour
+{
+    public UnityEvent OnPlayerInteractsHere;
+
+    private void OnTriggerStay()
+    {
+
+        if (PlayerController.isPlayerCanInteractBecauseHeLooksAtSmth_item && InputReceiver.CheckIf_Use_Pressed())
+        {
+            Debug.Log("UseKeyPRessed");
+            FireEvent();
+        }
+    }
+
+    public void FireEvent()
+    {
+        OnPlayerInteractsHere.Invoke();
+
+    }
+
+
+}
