8998b2e8cb209f1f4047696699f9a53fe21e395b
MININT-00KO2OI\Hardev <walter_sullivan@hotmail.de>
2022-12-27 17:02:20 +0100

    update detect and play footsteps logic

--- Assets/_Scripts/Audio Management/DetectAndPlayKindOfFootsteps.cs
+++ Assets/_Scripts/Audio Management/DetectAndPlayKindOfFootsteps.cs
@@ -8,27 +8,41 @@ public class DetectAndPlayKindOfFootsteps : MonoBehaviour
     FootStepsGetter footStepsGetter;
     AudioSource footStepSource;
 
+    //SO stats
+    public PlayerStats playerstats;
+
+    //_From which height of the chart raycast down to detect which kind of footsteps are in floor
+    public Transform footStepDetectPosition;
+
+    //filter by mask
     public LayerMask groundMask;
 
     private void Start()
     {
+        //cache the single audiosource responsible for footsteps at start
         footStepSource = GetComponentInChildren<AudioSource>();
     }
-    public void PlayFootStep()
+    public void DetectFootstepTypeAndPlay()
     {
         RaycastHit hit;
 
-        if (Physics.Raycast(transform.position, Vector3.down, out hit, groundMask))
+        if (Physics.Raycast(footStepDetectPosition.position, Vector3.down, out hit, playerstats._LineToStepDetectDistance, groundMask))
         {
- 
-            footStepsGetter = hit.transform.GetComponent<FootStepsGetter>();
+            footStepsGetter = hit.transform.gameObject.GetComponent<FootStepsGetter>();
+            //if(footStepsGetter != null)
             footStepSource.clip = footStepsGetter.stepType.footStepsClip;
 
             Debug.Log("Got the footstep!");
+            PlayFootSteps();
         }
+
+
+    }
+    void PlayFootSteps()
+    {
         footStepSource.pitch = Random.Range(0.9f, 1.1f);
         footStepSource.Play();
-    
     }
+
 
 }
