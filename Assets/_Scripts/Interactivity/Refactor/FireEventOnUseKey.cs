b77078bb85c600df21aa59c8ee21ec99a2eaec04
MININT-00KO2OI\Hardev <walter_sullivan@hotmail.de>
2023-11-19 19:45:34 +0100

    integrate FSM logic into Loose Event Firing

--- Assets/_Scripts/Interactivity/Refactor/FireEventOnUseKey.cs
+++ Assets/_Scripts/Interactivity/Refactor/FireEventOnUseKey.cs
@@ -2,30 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using NodeCanvas.StateMachines;
 
 
 public class FireEventOnUseKey : MonoBehaviour
 {
-    public UnityEvent OnPlayerInteractsHere;
+    //public static Event OnPlayerInteractsHere;
 
+    public FSMOwner owner;
 
-    private void OnTriggerStay()
+    public void SendNormalEvent()
+    {
+        owner.SendEvent("MyEvent");
+    }
+
+    private void Update()
     {
 
         if (PlayerController.isPlayerCanInteractBecauseHeLooksAtSmth_item && InputReceiver.CheckIf_Use_Pressed())
         {
-            FireEvent();
+           // FireEvent();
+            SendNormalEvent();
             Debug.Log("UseKeyPRessed");
         }
     }
 
     public void FireEvent()
     {
-        Debug.Log("ISEVENTNULL??? : " + (OnPlayerInteractsHere == null));
-        if (OnPlayerInteractsHere != null)
-        {
-            OnPlayerInteractsHere.Invoke();
-        }
+        //Debug.Log("ISEVENTNULL??? : " + (OnPlayerInteractsHere == null));
+        //if (OnPlayerInteractsHere != null)
+        //{
+        //    OnPlayerInteractsHere.Invoke();
+        //}
     }
 
 
